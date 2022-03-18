e70a31c074e2ba890b3ec57f108d6a6accfc6bfd
Kevin Sekgobela <kevinnoko23@gmail.com>
2022-03-18 18:03:34 +0200

    Added summary_characters.cpp and summary_characters.hpp
    Files contain declaration and definition for Summary_Characters Class
    that contain static methods. Methods retrieve data about the characters
    such as if they are digits or alphabetic. The methods only return
    booleans that represent if charactes are what method specifies.
    
    e.g If characters are only digits then  Summary_Characters::isDigit()
    would be true as all characters are digits. If not all of them were a digit then
    false would get returned instead.

---
+++ text_preccessing/characters/summary_characters.cpp
@@ -0,0 +1,68 @@
+#include "summary_characters.hpp"
+
+
+bool Summary_Characters::isAlnum(Characters chars_obj){
+    int true_ratio = Ratio_Characters::isAlnum(chars_obj);
+    return true_ratio == 1;
+}
+
+bool Summary_Characters::isAlpha(Characters chars_obj){
+    int true_ratio = Ratio_Characters::isAlpha(chars_obj);
+    return true_ratio == 1;
+}
+
+bool Summary_Characters::isASCII(Characters chars_obj){
+    int true_ratio = Ratio_Characters::isASCII(chars_obj);
+    return true_ratio == 1;
+}
+
+bool Summary_Characters::isBlank(Characters chars_obj){
+    int true_ratio = Ratio_Characters::isBlank(chars_obj);
+    return true_ratio == 1;
+}
+
+bool Summary_Characters::isCntrl(Characters chars_obj){
+    int true_ratio = Ratio_Characters::isCntrl(chars_obj);
+    return true_ratio == 1;
+}
+
+bool Summary_Characters::isDigit(Characters chars_obj){
+    int true_ratio = Ratio_Characters::isDigit(chars_obj);
+    return true_ratio == 1;
+}
+
+bool Summary_Characters::isGraph(Characters chars_obj){
+    int true_ratio = Ratio_Characters::isGraph(chars_obj);
+    return true_ratio == 1;
+}
+
+bool Summary_Characters::isLower(Characters chars_obj){
+    int true_ratio = Ratio_Characters::isLower(chars_obj);
+    return true_ratio == 1;
+}
+
+bool Summary_Characters::isPrint(Characters chars_obj){
+    int true_ratio = Ratio_Characters::isPrint(chars_obj);
+    return true_ratio == 1;
+}
+
+bool Summary_Characters::isPunct(Characters chars_obj){
+    int true_ratio = Ratio_Characters::isPunct(chars_obj);
+    return true_ratio == 1;
+}
+
+bool Summary_Characters::isSpace(Characters chars_obj){
+    int true_ratio = Ratio_Characters::isSpace(chars_obj);
+    return true_ratio == 1;
+}
+
+bool Summary_Characters::isUpper(Characters chars_obj){
+    int true_ratio = Ratio_Characters::isUpper(chars_obj);
+    return true_ratio == 1;
+}
+
+bool Summary_Characters::isXdigit(Characters chars_obj){
+    int true_ratio = Ratio_Characters::isXdigit(chars_obj);
+    return true_ratio == 1;
+}
+
---
+++ text_preccessing/characters/summary_characters.hpp
@@ -0,0 +1,105 @@
+#include "ratio_characters.hpp"
+
+/**
+ * @brief Most common Character which is simiar to Spacy tokens and programming strings.
+ * They use stored characters to determine if characters as whole are didgits, alpha
+ * or other types.
+ * Think of them as summary of all characters.
+ * They contain almost all methods character but themselfs are based on all characters.
+ * If characters contains digits and alphabets then the characters are considered
+ * Alphanumeric.
+ */
+class Summary_Characters{
+    public:
+        /**
+         * @brief returns booleans representing if each character is ASCII
+         * @param chars_obj object of type Characters
+         * 
+         * @return bool number between 0 and 1
+         */
+        static bool isASCII(Characters chars_obj);
+        /**
+         * @brief returns booleans representing if each character is alphanumeric
+         * @param chars_obj object of type Characters
+         * 
+         * @return bool number between 0 and 1
+         */
+        static bool isAlnum(Characters chars_obj);
+        /**
+         * @brief returns booleans representing if each character is alphabetic
+         * @param chars_obj object of type Characters
+         * 
+         * @return bool number between 0 and 1
+         */
+        static bool isAlpha(Characters chars_obj);
+        /**
+         * @brief returns booleans representing if each character is blank character
+         * @param chars_obj object of type Characters
+         * 
+         * @return bool number between 0 and 1
+         */
+        static bool isBlank(Characters chars_obj);
+        /**
+         * @brief returns booleans representing if each character is control character
+         * @param chars_obj object of type Characters
+         * 
+         * @return bool number between 0 and 1
+         */
+        static bool isCntrl(Characters chars_obj);
+        /**
+         * @brief returns booleans representing if each character is digit
+         * @param chars_obj object of type Characters
+         * 
+         * @return bool number between 0 and 1
+         */
+        static bool isDigit(Characters chars_obj);
+        /**
+         * @brief returns booleans representing if each character is in lowercase
+         * @param chars_obj object of type Characters
+         * 
+         * @return bool number between 0 and 1
+         */
+        static bool isLower(Characters chars_obj);
+        /**
+         * @brief returns booleans representing if each character is printable
+         * @param chars_obj object of type Characters
+         * 
+         * @return bool number between 0 and 1
+         */
+        static bool isPrint(Characters chars_obj);
+        /**
+         * @brief returns booleans representing if each character is punctuation
+         * @param chars_obj object of type Characters
+         * 
+         * @return bool number between 0 and 1
+         */
+        static bool isPunct(Characters chars_obj);
+        /**
+         * @brief returns booleans representing if each character is space character
+         * @param chars_obj object of type Characters
+         * 
+         * @return bool number between 0 and 1
+         */
+        static bool isSpace(Characters chars_obj);
+        /**
+         * @brief returns booleans representing if each character is in uppercase
+         * @param chars_obj object of type Characters
+         * 
+         * @return bool number between 0 and 1
+         */
+        static bool isUpper(Characters chars_obj);
+        /**
+         * @brief returns booleans representing if each character is valid hexadecimal character
+         * @param chars_obj object of type Characters
+         * 
+         * @return bool number between 0 and 1
+         */
+        static bool isXdigit(Characters chars_obj);
+        /**
+         * @brief returns booleans representing if each character is graphical character
+         * @param chars_obj object of type Characters
+         * 
+         * @return bool number between 0 and 1
+         */
+        static bool isGraph(Characters chars_obj);
+};
