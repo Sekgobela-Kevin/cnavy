c965bf54fbfa4e29dc5cf5991e2592119c477995
Kevin Sekgobela <kevinnoko23@gmail.com>
2022-03-24 10:35:38 +0200

    Added bool_set.cpp and bool_set.hpp containing Bool_Set Class
    Bool_Set Class is just a class to emulate dynamic BitSet of C++ Standard Library.
    Since text processing part of the application uses collection of booleans, theres
    a need for dynamic bitset which is not available is STL.The Bool_Set uses Vector
    to store booleans and each operation on bools results in iteration.
    
    All methgods are static so that they can be accessed without creating instance of Class.
    Class has no constructor as alll of its methods are static.

---
+++ text_preccessing/characters/bool_set.cpp
@@ -0,0 +1,78 @@
+ #include <functional>   
+#include <algorithm>    
+#include <string>  
+#include "bool_set.hpp"
+
+
+bool_chars_type Bool_Set::for_each_elem(bool_chars_type bool_collection, 
+std::function<bool(bool)> input_func){
+    bool_chars_type booleans;
+    booleans.reserve(bool_collection.size());
+    for (bool obj : bool_collection)
+    {
+        booleans.push_back(input_func(obj));
+    }
+    return  booleans;
+}
+
+bool_chars_type Bool_Set::for_each_elem(bool_chars_type collection, bool_chars_type collection2,
+std::function<bool(bool, bool)> input_func){
+    bool_chars_type booleans;
+    booleans.reserve(collection.size());
+    // iteration will happen to all elements in both collection 
+    // Even if their sizes are different
+    for (size_t i = 0; i < collection.size() || i < collection2.size(); i++)
+    {
+        if(i > collection.size() || i > collection2.size()){
+            // one of collections is out of range
+            // false is pushed since no logical operation that was performed
+            booleans.push_back(false);
+            continue;
+        }
+        else{
+            booleans.push_back(input_func(collection[i], collection2[i]));
+        }
+    }
+    return  booleans;
+}
+
+
+bool_chars_type Bool_Set::boolAND(bool_chars_type collec, bool_chars_type collec2){
+    return Bool_Set::for_each_elem(collec, collec2, std::logical_and<bool>());
+}
+
+bool_chars_type Bool_Set::boolOR(bool_chars_type collec, bool_chars_type collec2){
+    return Bool_Set::for_each_elem(collec, collec2, std::logical_or<bool>());
+}
+
+bool_chars_type Bool_Set::boolXOR(bool_chars_type collec, bool_chars_type collec2){
+    return Bool_Set::for_each_elem(collec, collec2, [](bool a, bool b){return a ^ b;});
+}
+
+bool_chars_type Bool_Set::boolNOT(bool_chars_type collec){
+    return Bool_Set::for_each_elem(collec, std::logical_not<bool>());
+}
+
+
+bool Bool_Set::all(bool_chars_type collec){
+    return std::count(collec.begin(), collec.end(), true) == collec.size();
+}
+
+
+bool Bool_Set::any(bool_chars_type collec){
+    return std::count(collec.begin(), collec.end(), true) == 0;
+}
+
+bool Bool_Set::count(bool_chars_type collec){
+    return std::count(collec.begin(), collec.end(), true);
+}
+
+std::string Bool_Set::toString(bool_chars_type collec){
+    std::string str;
+    str.reserve(collec.size());
+    for (bool elem : collec)
+    {
+        str.push_back(elem ? '1' : '0');
+    }
+    return str;
+}
---
+++ text_preccessing/characters/bool_set.hpp
@@ -0,0 +1,97 @@
+#pragma once 
+#include <string>
+#include <vector>
+#include <functional>
+
+// collection of bools e.g {true, false, ..}
+typedef std::vector<bool> bool_chars_type;
+// collection of containing collection of bools e.g {{tru, false}, ...}
+typedef std::vector<std::vector<bool>> bool_matric_type;
+
+class Bool_Set{
+    public:
+        /**
+         * @brief performs operation on each element of bool collection and return results
+         * 
+         * @param bool_collection collection of booleans
+         * @param input_func funtion to call on each element
+         * @return bool_chars_type 
+         */
+        static bool_chars_type for_each_elem(bool_chars_type bool_collection, 
+            std::function<bool(bool)> input_func);
+        /**
+         * @brief performs operation based on each elements of bool collections and return results
+         * 
+         * @param collection collection of booleans
+         * @param collection2 collection of booleans
+         * @param input_func funtion to call on each element
+         * @return bool_chars_type 
+         */
+        static bool_chars_type for_each_elem(bool_chars_type collection,  bool_chars_type collection2,
+            std::function<bool(bool, bool)> input_func);
+        /**
+         * @brief Performs bitwise AND operation on bool_collection
+         * 
+         * @param bool_collection collection of booleans to perform operation
+         * @param bool_collection2 collection of booleans apply on bool_collection arg
+         * @return bool_chars_type 
+         */
+        static bool_chars_type boolAND(bool_chars_type bool_collection, bool_chars_type bool_collection2);
+        /**
+         * @brief Performs bitwise OR operation on bool_collection
+         * 
+         * @param bool_collection collection of booleans to perform operation
+         * @param bool_collection2 collection of booleans apply on bool_collection arg
+         * @return bool_chars_type 
+         */
+        static bool_chars_type boolOR(bool_chars_type bool_collection, bool_chars_type bool_collection2);
+        /**
+         * @brief Performs bitwise XOR operation on bool_collection
+         * 
+         * @param bool_collection collection of booleans to perform operation
+         * @param bool_collection2 collection of booleans apply on bool_collection arg
+         * @return bool_chars_type 
+         */
+        static bool_chars_type boolXOR(bool_chars_type bool_collection, bool_chars_type bool_collection2);
+        /**
+         * @brief Performs bitwise NOT operation on bool_collection
+         * 
+         * @param bool_collection collection of booleans to perform operation
+         * @param bool_collection2 collection of booleans apply on bool_collection arg
+         * @return bool_chars_type 
+         */
+        static bool_chars_type boolNOT(bool_chars_type bool_collection);
+
+        /**
+         * @brief Tests whether all bits from bool_collection  are set or not.
+         * 
+         * @param bool_collection 
+         * @return true 
+         * @return false 
+         */
+        static bool any(bool_chars_type bool_collection);
+        /**
+         * @brief Tests whether all bits from bool_collection  are set or not.
+         * 
+         * @param bool_collection 
+         * @return true 
+         * @return false 
+         */
+        static bool all(bool_chars_type bool_collection);
+        /**
+         * @brief Count number of set bits from bitset.
+         * 
+         * @param bool_collection 
+         * @return true 
+         * @return false 
+         */
+        static bool count(bool_chars_type bool_collection);
+        /**
+        * @brief Converts bool_collection to string object.
+        * 
+        * @param bool_collection 
+        * @return true 
+        * @return false 
+        */
+        std::string static toString(bool_chars_type bool_collection);
+};
