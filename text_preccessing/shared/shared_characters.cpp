f958fcc7385171ba2425044f1cc60653cd0f7a6a
Kevin Sekgobela <kevinnoko23@gmail.com>
2022-03-28 22:00:09 +0200

    Added Shared_Characters Class source, header and test files
    Shared_Character class is used to create instance that will contain Character objects
    that are shared by multiple Characters objects. Think of it as central place were
    Character objects get stored and everyone that needs certain Character object references
    it instead of creating one construtor.. This would save thousants if not millions of Character
    objects that could have been had to be manually created.
    
    Object of this class can be created from string and charcters objects but anything
    that contain data that can be used to create CHaracter objects could work if
    implemented. For string, duplicate characters get removed and resultiong characters
    are checked if already exists in stored map(char, Character obj). If character does
    not already exists in map then Character object is created and then stored with character
    being key and that CHaracter object being the value.
    
    It was observed from task manager that memory was saved as only few MBs of RAM was used instead
    of 1000 MBs when creating characters object from string. It did save a lot of memory which seems
    to be promising and rewarding at the end. Problem is that it takes some time to remove duplicates
    from string and it takes seconds to work with long strings with lot of this time spent checking
    if element exists or removing duplicate characters from string. This was tested on 100 million
    characters(same character) which to ok about 30 seconds but improved to 20 seconds.
    
    This same thing should be applied to Tokens and Characters objects so that memory can be saved
    when creating document object.

---
+++ text_preccessing/shared/shared_characters.cpp
@@ -0,0 +1,78 @@
+#include "shared_characters.hpp"
+#include <unordered_set>
+#include <set>
+
+Shared_Characters::Shared_Characters(std::string input_string){
+    this->add(input_string);
+}
+
+Shared_Characters::Shared_Characters(Characters chars_obj){
+    this->add(chars_obj);
+}
+
+void Shared_Characters::add(char_type char_){
+    if(!this->exists(char_)){
+        Character char_obj(char_);
+        this->characters_map.emplace(char_, char_obj);
+    }
+}
+
+void Shared_Characters::add(Character char_obj){
+    char_type char_ = char_obj.getCurrentChar();
+    if(!this->exists(char_)){
+        this->characters_map.emplace(char_, char_obj);
+    }
+}
+
+void Shared_Characters::add(std::string input_string){
+    //std::string chars_set = unique(input_string);
+    //printf("unique string created with size %d\n", chars_set.size());
+    for(char_type character : input_string){
+        this->add(character);
+    }
+}
+
+void Shared_Characters::add(Characters chars_obj){
+    characters_objs_type char_objs = chars_obj.getCharsObjs();
+    for(Character char_obj : char_objs){
+        this->add(char_obj);
+    }
+}
+
+void Shared_Characters::remove(char_type char_){
+    this->characters_map.erase(char_);
+}
+
+void Shared_Characters::remove(std::string input_string){
+    for(char_type character : this->unique(input_string)){
+        this->remove(character);
+    }
+}
+
+bool Shared_Characters::exists(char_type char_){
+    return this->characters_map.count(char_);
+}
+
+void Shared_Characters::clear(){
+    this->characters_map.clear();
+}
+
+size_t Shared_Characters::size(){
+    return this->characters_map.size();
+}
+
+bool Shared_Characters::empty(){
+    return this->characters_map.empty();
+}
+
+std::string Shared_Characters::unique(std::string input_string){
+    size_t size = input_string.length();
+    std::set<char_type> chars_set;
+    for( unsigned i = 0; i < size; ++i ) chars_set.insert( input_string[i] );
+    input_string.assign(chars_set.begin(), chars_set.end());
+    return input_string;
+
+
+}
+
+
