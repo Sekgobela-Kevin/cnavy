e17d44ca245054c9856ee397894068e166d61eff
Kevin Sekgobela <kevinnoko23@gmail.com>
2022-04-02 18:55:45 +0200

    updated tests for Reference_Token Class
    Reference_Token Class does not have any future waiting for it. It was intended to reference attributes
    of CHaracters_Token in hope of saving memory through references. That was far from being true since
    a reference uses memory of 4 bytes(same as int and pointers). Each reference used 4 bytes which is much
    compared to most  attributes using 1 byte as they are mostly bools. It does not create any performance
    benefits but degrates as it depends on another object. Its not worth to use the object both practically
    and theoritically.
    
    References and pointers will save memory if referenced object is large and that 4 bytes can be worth
    compared to object of size megabytes. Instead of referencing individual attributes rather reference
    the whole object. It was cause by my lack of practical knowledge of references and memory address
    including how much memory occupied. Intensions was to optimise memory and performance so that multiple
    documemets can be processed at the same time.
    
    For now it will just be kept and its purpose will be replaced by the Class that it was referencing
    it objects attributes. Characters_Token Class will take its place up until Tokens get created.

--- text_preccessing/token/test/reference_token_test.cpp
+++ text_preccessing/token/test/reference_token_test.cpp
@@ -1,37 +1,51 @@
 #include <iostream>
 #include <cassert>
 #include <string>
+#include <memory>
 #include "../reference_token.hpp"
 using namespace std;
 
 /*
  g++ ..\character\character.cpp ..\character\character_compute.cpp 
- characters.cpp bool_characters.cpp ratio_characters.cpp 
- summary_characters_test.cpp bool_set.cpp  static_Reference_Token.cpp 
- Reference_Token.cpp ..\token\characters_token.cpp ..\token\reference_token.cpp
+ characters.cpp bool_set.cpp bool_characters.cpp ratio_characters.cpp 
+ summary_characters.cpp ..\token\characters_token.cpp ..\token\reference_token.cpp
  ..\token\test\reference_token_test.cpp 
 */
 
+
 void Reference_Token_Test(){
     clog << "Test: Started Reference_Token Class Test using" << endl;
-    string input_string(4, 'c');
+    string* input_string = new string("token");
 
     string obj_input_string;
     string obj2_input_string;
 
-    cout << "Enter text: ";
-    cin >> obj_input_string;
+
    
 
     vector<string> attr = {"chars", "is_digit", "is_alpha"};
-    Characters* chars_obj = new Characters(obj_input_string);
+    Characters* chars_obj = new Characters(*input_string);
+    vector<std::reference_wrapper<Reference_Token>> chars_tokens;
 
     Characters_Token* chars_tok = new Characters_Token(*chars_obj);
-    Reference_Token* ref_tok = new Reference_Token(*chars_tok);
+    Reference_Token ref_tok = Reference_Token(*chars_tok);
 
-    cout << "lower: " << ref_tok->lower << endl;
-    ref_tok->lower = "chaned";
-    cout << "isUpper: " << (*ref_tok).isUpper(0.5) << endl;
+    
+    size_t count = 20000000;
+    for (size_t i = 0; i < count; i++)
+    {
+        chars_tokens.push_back(ref_tok);
+    }
+    
+ 
+
+    cout << "size of vector of chars_ref is " << chars_tokens.size() << " " << ", items size " <<
+    input_string->size() << " and " << chars_tokens.size()*input_string->size() << " chars obj"<<  endl;
+
+
+    cout << chars_tokens[3].get().is_alpha << endl;
+    delete input_string;
+    while(true);
     
 
 
