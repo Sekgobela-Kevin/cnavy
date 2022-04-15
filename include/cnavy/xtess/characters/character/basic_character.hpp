9fdf52cad3cb32e795c5629a86f5e17b8e4e900c
Sekgobela-Kevin <kevinnoko23@gmail.com>
2022-04-15 17:56:32 +0200

    Added Basic_Character class
    Basic_Character objects are intended to be used with Basic_Token which will
    then be used with Basic_Tokeniser. They are all built on top of other characters
    with Basic_Character built from Character and Basic_Token from Raw_Token and then
    Basic_Tokeniser from Raw_Tokeniser.
    
    They are just meant to improve interaction with undelying character or offer performance
    boost.

---
+++ include/cnavy/xtess/characters/character/basic_character.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "character.hpp"
+
+/**
+ * @brief creates character objects to be used with Basic_Token
+ * 
+ */
+class Basic_Character : public Character{
+    using Character::Character;
+};
